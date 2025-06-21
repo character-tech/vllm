d1c43513741e8d21196b92b8e8f8e0e703e8dd61
Your Name <you@example.com>
2025-06-21 16:54:28 +0000

    metrics

--- vllm/entrypoints/openai/serving_completion.py
+++ vllm/entrypoints/openai/serving_completion.py
@@ -139,7 +139,7 @@ class OpenAIServingCompletion(OpenAIServing):
         
             yield "data: [DONE]\n\n"
 
-            report_metrics(request, output)
+            report_metrics(request, output, final)
     
         return _chunk_generator()
 
